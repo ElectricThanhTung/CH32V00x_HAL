c71cdfd1941cd77a4b2efe7e2c49682260769465
Thanh Tùng <electricthanhtung@gmail.com>
2023-12-04 21:09:49 +0700

    Update compiler header file

--- Drivers/Core/Inc/compiler.h
+++ Drivers/Core/Inc/compiler.h
@@ -17,24 +17,28 @@
     #define __STATIC_FORCEINLINE    __forceinline static inline
     #define __INTERRUPT             __attribute__((interrupt()))
     #define __WEAK                  __attribute__((weak))
+    #define __USED                  __attribute__((used))
 #elif defined(__ICCARM__)
     #define __ASM                    __asm                  /*!< asm keyword for IAR Compiler          */
     #define __INLINE                inline                  /*!< inline keyword for IAR Compiler. Only avaiable in High optimization mode! */
     #define __STATIC_FORCEINLINE    _Pragma("inline=forced") static inline
     #define __INTERRUPT             __attribute__((interrupt()))
     #define __WEAK                  __attribute__((weak))
+    #define __USED                  __attribute__((used))
 #elif defined(__GNUC__)
     #define __ASM                   __asm                   /*!< asm keyword for GNU Compiler          */
     #define __INLINE                inline                  /*!< inline keyword for GNU Compiler       */
     #define __STATIC_FORCEINLINE    __attribute__((always_inline)) static inline
     #define __INTERRUPT             __attribute__((interrupt()))
     #define __WEAK                  __attribute__((weak))
+    #define __USED                  __attribute__((used))
 #elif defined(__TASKING__)
     #define __ASM                   __asm                   /*!< asm keyword for TASKING Compiler      */
     #define __INLINE                inline                  /*!< inline keyword for TASKING Compiler   */
     #define __STATIC_FORCEINLINE    __attribute__((always_inline)) static inline
     #define __INTERRUPT             __attribute__((interrupt()))
     #define __WEAK                  __attribute__((weak))
+    #define __USED                  __attribute__((used))
 #endif
 
 #endif /* __CONPILER_H */
