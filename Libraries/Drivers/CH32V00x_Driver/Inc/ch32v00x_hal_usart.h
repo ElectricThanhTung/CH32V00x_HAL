2a26b24e4a0b13581a6108d8ee1ff0e307a4e596
Thanh Tùng <electricthanhtung@gmail.com>
2023-12-14 11:06:19 +0700

    Update USART library

--- Libraries/Drivers/CH32V00x_Driver/Inc/ch32v00x_hal_usart.h
+++ Libraries/Drivers/CH32V00x_Driver/Inc/ch32v00x_hal_usart.h
@@ -4,62 +4,108 @@
 
 #include "ch32v00x_hal.h"
 
-typedef enum {
-    USART_MODE_RX = USART_CTLR1_TE,
-    USART_MODE_TX = USART_CTLR1_RE,
-    USART_MODE_TX_RX = USART_MODE_RX | USART_MODE_TX
-} USART_ModeTypeDef;
+typedef struct {
+public:
+    __IO uint16_t STATR;
+private:
+    __IO uint16_t RESERVED0;
+public:
+    __IO uint16_t DATAR;
+private:
+    __IO uint16_t RESERVED1;
+public:
+    __IO uint16_t BRR;
+private:
+    __IO uint16_t RESERVED2;
+public:
+    __IO uint16_t CTLR1;
+private:
+    __IO uint16_t RESERVED3;
+public:
+    __IO uint16_t CTLR2;
+private:
+    __IO uint16_t RESERVED4;
+public:
+    __IO uint16_t CTLR3;
+private:
+    __IO uint16_t RESERVED5;
+public:
+    __IO uint16_t GPR;
+private:
+    __IO uint16_t RESERVED6;
+} USART_RegsTypeDef;
 
-typedef enum {
-    USART_WORD_LENGTH_8 = 0U,
-    USART_WORD_LENGTH_9 = USART_CTLR1_M
-} USART_WordLengthTypeDef;
+class USART_RxModeTypeDef {
+private:
+    USART_RegsTypeDef REGS;
+public:
+    void Enable(void);
+    void Disable(void);
+private:
+    USART_RxModeTypeDef(void);
+    USART_RxModeTypeDef(USART_RxModeTypeDef &);
+};
 
-typedef enum {
-    USART_PARITY_NONE = 0U,
-    USART_PARITY_EVEN = USART_CTLR1_PCE,
-    USART_PARITY_ODD = USART_CTLR1_PCE | USART_CTLR1_PS
-} USART_ParityTypeDef;
+class USART_TxModeTypeDef {
+private:
+    USART_RegsTypeDef REGS;
+public:
+    void Enable(void);
+    void Disable(void);
+private:
+    USART_TxModeTypeDef(void);
+    USART_TxModeTypeDef(USART_TxModeTypeDef &);
+};
+
+class USART_BaudrateTypeDef {
+private:
+    USART_RegsTypeDef REGS;
+public:
+    uint32_t SetValue(uint32_t baudRate);
+    uint32_t GetValue(void);
+private:
+    USART_BaudrateTypeDef(void);
+    USART_BaudrateTypeDef(USART_BaudrateTypeDef &);
+};
+
+class USART_WordLengthTypeDef {
+private:
+    USART_RegsTypeDef REGS;
+public:
+    void Mode8Bit(void);
+    void Mode9Bit(void);
+    bool IsMode8Bit(void);
+    bool IsMode9Bit(void);
+private:
+    USART_WordLengthTypeDef(void);
+    USART_WordLengthTypeDef(USART_WordLengthTypeDef &);
+};
+
+class USART_ParityTypeDef {
+private:
+    USART_RegsTypeDef REGS;
+public:
+    void Disable(void);
+    void Even(void);
+    void Odd(void);
+private:
+    USART_ParityTypeDef(void);
+    USART_ParityTypeDef(USART_ParityTypeDef &);
+};
 
 class USART_TypeDef {
 public:
-    struct {
-    public:
-        __IO uint16_t STATR;
-    private:
-        __IO uint16_t RESERVED0;
-    public:
-        __IO uint16_t DATAR;
-    private:
-        __IO uint16_t RESERVED1;
-    public:
-        __IO uint16_t BRR;
-    private:
-        __IO uint16_t RESERVED2;
-    public:
-        __IO uint16_t CTLR1;
-    private:
-        __IO uint16_t RESERVED3;
-    public:
-        __IO uint16_t CTLR2;
-    private:
-        __IO uint16_t RESERVED4;
-    public:
-        __IO uint16_t CTLR3;
-    private:
-        __IO uint16_t RESERVED5;
-    public:
-        __IO uint16_t GPR;
-    private:
-        __IO uint16_t RESERVED6;
-    } REGS;
+    union {
+        USART_RegsTypeDef REGS;
+        USART_RxModeTypeDef RxMode;
+        USART_TxModeTypeDef TxMode;
+        USART_BaudrateTypeDef BaudRate;
+        USART_WordLengthTypeDef WordLength;
+        USART_ParityTypeDef Parity;
+    };
 public:
     void EnableClock(void);
     void DisableClock(void);
-    void SetMode(USART_ModeTypeDef mode);
-    uint32_t SetBaudRate(uint32_t baudRate);
-    void SetWordLength(USART_WordLengthTypeDef wordLength);
-    void SetParity(USART_ParityTypeDef parity);
     HAL_StatusTypeDef Transmit(uint8_t *txData, uint16_t txLength, uint32_t timeout = 0xFFFFFFFFUL);
     HAL_StatusTypeDef Transmit(uint16_t *txData, uint16_t txLength, uint32_t timeout = 0xFFFFFFFFUL);
     HAL_StatusTypeDef Transmit(uint16_t txData, uint32_t timeout = 0xFFFFFFFFUL);
--- Libraries/Drivers/CH32V00x_Driver/Src/ch32v00x_hal_usart.cpp
+++ Libraries/Drivers/CH32V00x_Driver/Src/ch32v00x_hal_usart.cpp
@@ -2,6 +2,117 @@
 #include "ch32v00x_hal_usart.h"
 
 /**
+ * @brief  Enable USART Rx mode.
+ * @retval None.
+ */
+void USART_RxModeTypeDef::Enable(void) {
+    REGS.CTLR1 |= USART_CTLR1_RE;
+}
+
+/**
+ * @brief  Disable USART Rx mode.
+ * @retval None.
+ */
+void USART_RxModeTypeDef::Disable(void) {
+    REGS.CTLR1 &= ~USART_CTLR1_RE;
+}
+
+/**
+ * @brief  Enable USART Tx mode.
+ * @retval None.
+ */
+void USART_TxModeTypeDef::Enable(void) {
+    REGS.CTLR1 |= USART_CTLR1_TE;
+}
+
+/**
+ * @brief  Disable USART Tx mode.
+ * @retval None.
+ */
+void USART_TxModeTypeDef::Disable(void) {
+    REGS.CTLR1 &= ~USART_CTLR1_TE;
+}
+
+/**
+ * @brief  Set baudrate for USART according to the specified parameters in the baudRate.
+ * @param  baudRate specifies the baudRate used for USART.
+ * @note   This function will automatically calculate the best value to assign to the
+ *         USARTx_BRR register based on the input baudrate parameter. It will also return
+ *         the actual baudrate value based on the currently configured BRR value.
+ * @retval Actual baudrate value.
+ */
+uint32_t USART_BaudrateTypeDef::SetValue(uint32_t baudRate) {
+    uint32_t hclk = RCC.HCLK.GetFreq();
+    uint32_t brr = (hclk + (baudRate / 2)) / baudRate;
+    REGS.BRR = brr;
+    return hclk / brr;
+}
+
+/**
+ * @brief  return the actual baudrate value based on the currently configured BRR value.
+ * @retval Actual baudrate value.
+ */
+uint32_t USART_BaudrateTypeDef::GetValue(void) {
+    return RCC.HCLK.GetFreq() / REGS.BRR;
+}
+
+/**
+ * @brief  Set word length to 8-bits data for USART.
+ * @retval None.
+ */
+void USART_WordLengthTypeDef::Mode8Bit(void) {
+    REGS.CTLR1 &= ~USART_CTLR1_M;
+}
+
+/**
+ * @brief  Set word length to 9-bits data for USART.
+ * @retval None.
+ */
+void USART_WordLengthTypeDef::Mode9Bit(void) {
+    REGS.CTLR1 |= USART_CTLR1_M;
+}
+
+/**
+ * @brief  Check whether the current USART configuration is 8-bit mode or not.
+ * @retval Boolean.
+ */
+bool USART_WordLengthTypeDef::IsMode8Bit(void) {
+    return (REGS.CTLR1 & USART_CTLR1_M) == 0U;
+}
+
+/**
+ * @brief  Check whether the current USART configuration is 9-bit mode or not.
+ * @retval Boolean.
+ */
+bool USART_WordLengthTypeDef::IsMode9Bit(void) {
+    return (REGS.CTLR1 & USART_CTLR1_M) == USART_CTLR1_M;
+}
+
+/**
+ * @brief  Disable parity for USART.
+ * @retval None.
+ */
+void USART_ParityTypeDef::Disable(void) {
+    REGS.CTLR1 &= ~(USART_CTLR1_PCE | USART_CTLR1_PS);
+}
+
+/**
+ * @brief  Set parity to even mode for USART.
+ * @retval None.
+ */
+void USART_ParityTypeDef::Even(void) {
+    REGS.CTLR1 = (REGS.CTLR1 & ~USART_CTLR1_PS) | USART_CTLR1_PCE;
+}
+
+/**
+ * @brief  Set parity to odd mode for USART.
+ * @retval None.
+ */
+void USART_ParityTypeDef::Odd(void) {
+    REGS.CTLR1 |= USART_CTLR1_PCE | USART_CTLR1_PS;
+}
+
+/**
  * @brief  Enable the USART1 peripheral clock.
  * @note   This function will use RCC module to enable clock for USART1 peripheral.
  * @retval None.
@@ -22,61 +133,18 @@ void USART_TypeDef::DisableClock(void) {
 }
 
 /**
- * @brief  Set mode for USART according to the specified parameters in the mode.
- * @param  mode specifies the mode to be set for USART.
- * @retval None.
- */
-void USART_TypeDef::SetMode(USART_ModeTypeDef mode) {
-    REGS.CTLR1 = (REGS.CTLR1 & ~(USART_CTLR1_TE | USART_CTLR1_RE)) | mode;
-}
-
-/**
- * @brief  Set baudrate for USART according to the specified parameters in the baudRate.
- * @param  baudRate specifies the baudRate used for USART.
- * @note   This function will automatically calculate the best value to assign to the
- *         USARTx_BRR register based on the input baudrate parameter. It will also return
- *         the actual baudrate value based on the currently configured BRR value.
- * @retval Actual baudrate value.
- */
-uint32_t USART_TypeDef::SetBaudRate(uint32_t baudRate) {
-    uint32_t hclk = RCC.HCLK.GetFreq();
-    uint32_t brr = (hclk + (baudRate / 2)) / baudRate;
-    REGS.BRR = brr;
-    return hclk / brr;
-}
-
-/**
- * @brief  Set word length 8 bits or 9 bits data for USART according to the specified
- *         parameters in the wordLength.
- * @param  wordLength specifies the word length of 8 bits or 9 bits data for for USART.
- * @retval None.
- */
-void USART_TypeDef::SetWordLength(USART_WordLengthTypeDef wordLength) {
-    REGS.CTLR1 = (REGS.CTLR1 & ~USART_CTLR1_M) | wordLength;
-}
-
-/**
- * @brief  Set parity for USART according to the specified parameters in the parity.
- * @param  parity specifies the parity to be set for USART.
- * @retval None.
- */
-void USART_TypeDef::SetParity(USART_ParityTypeDef parity) {
-    REGS.CTLR1 = (REGS.CTLR1 & ~(USART_CTLR1_PCE | USART_CTLR1_PS)) | parity;
-}
-
-/**
  * @brief  Transmit an amount of 8 bits array data in blocking mode.
  * @param  txData pointer to transmission data buffer.
  * @param  txLength the length of the data array to be transmitted.
  * @param  timeout timeout duration.
  * @note   Transmit mode must be enabled and the word length for USART must
- *         be set to USART_WORD_LENGTH_8. Otherwise, this function cannot
+ *         be set to 8-bit mode. Otherwise, this function cannot
  *         be executed and will return HAL_ERROR.
  * @retval HAL status.
  */
 HAL_StatusTypeDef USART_TypeDef::Transmit(uint8_t *txData, uint16_t txLength, uint32_t timeout) {
     uint32_t startTick = HAL.GetTickMs();
-    if(!(REGS.CTLR1 & USART_CTLR1_TE) || (REGS.CTLR1 & USART_CTLR1_M))
+    if(!(REGS.CTLR1 & USART_CTLR1_TE) || !WordLength.IsMode8Bit())
         return HAL_ERROR;
     while(txLength) {
         if(REGS.STATR & USART_STATR_TXE) {
@@ -96,13 +164,13 @@ HAL_StatusTypeDef USART_TypeDef::Transmit(uint8_t *txData, uint16_t txLength, ui
  * @param  txLength the length of the data array to be transmitted.
  * @param  timeout timeout duration.
  * @note   Transmit mode must be enabled and the word length for USART must
- *         be set to USART_WORD_LENGTH_9. Otherwise, this function cannot
+ *         be set to 9-bit mode. Otherwise, this function cannot
  *         be executed and will return HAL_ERROR.
  * @retval HAL status.
  */
 HAL_StatusTypeDef USART_TypeDef::Transmit(uint16_t *txData, uint16_t txLength, uint32_t timeout) {
     uint32_t startTick = HAL.GetTickMs();
-    if(!(REGS.CTLR1 & USART_CTLR1_TE) || !(REGS.CTLR1 & USART_CTLR1_M))
+    if(!(REGS.CTLR1 & USART_CTLR1_TE) || !WordLength.IsMode9Bit())
         return HAL_ERROR;
     while(txLength) {
         if(REGS.STATR & USART_STATR_TXE) {
